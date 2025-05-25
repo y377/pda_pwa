a6a44261acf1a901a4ac5561a189c4f49ece666b
y377 <58632405+y377@users.noreply.github.com>
2025-05-25 15:01:27 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -18,14 +18,92 @@ const urlsToCache = [
   'https://lib.baomitu.com/highlight.js/11.10.0/highlight.min.js'
 ];
 
+// 配件数据
+const partsData = {
+  brandMap: {
+    'AOC线缆': ['HG GENUINE', 'INNOLIGHT', 'Hisense', 'WTD'],
+    '光模块': ['HG GENUINE', 'INNOLIGHT', 'Hisense', 'Huawei', 'Finisar', 'WTD'],
+    '内存': ['Samsung', 'Hynix', 'Micron'],
+    '硬盘': ['Seagate', 'Western Digital', 'Intel', 'Samsung', 'Micron', 'KIOXIA', 'Solidigm'],
+    'CPU': ['Intel']
+  },
+  pnDataMap: {
+    '光模块': {
+      'HG GENUINE': ['MTRS-01X11-G', 'MQD-12F1C', 'MQD-56F2C', 'MTRQ-4S101'],
+      'INNOLIGHT': ['T-OL8CNT-N00', 'T-DQ8FNS-N01', 'T-DQ8FNS-N00', 'TR-FC13T-N00', 'TR-ZC13T-N00', 'T-DQ4CNT-N00', 'TR-FC13R-N00'],
+      'WTD': ['RTXM500-410', 'RTXM420-431', 'RTXM500-560', 'RTXM600-230', 'OEO-M100-73-13-Q28', 'OEO-M100-62-13-Q28', 'RTXM228-551', 'RTXM228-401', 'RTXM290-806', 'RTXM420-550'],
+      'Finisar': ['FTCD8613E1PCM', 'FTLX8574D3BCL', 'FTCD8613E2PCM-BY', 'FTCE4717E1PCB-BY', 'FTLX1475D3BCL'],
+      'Huawei': ['OM3680SX200', 'OM3660FX102'],
+      'Hisense': ['LMQ8811-PC+', 'LMS3821L-PCS', 'LTF8502-BC+', 'LMS3826-PC+', 'LTA1328-PC+'],
+      'Crealights': ['B5CLOS800SR8'],
+      'H3C': ['SFP-XG-LX-SM1310']
+    },
+    'AOC线缆': {
+      'HG GENUINE': ['ATRP-B005', 'ATRP-B007', 'ATRP-B010', 'ATRP-B020'],
+      'INNOLIGHT': ['C-PD2FNM005-N00', 'C-PD2FNM007-N00', 'C-PD2FNM010-N00'],
+      'Hisense': ['DMM8211-DC05', 'DMM8211-DC07', 'DMM8211-DC10', 'DMM8211-DC20'],
+      'WTD': ['RTXM520-105', 'RTXM520-107', 'RTXM520-110', 'RTXM520-120', 'RTXM500-905', 'RTXM500-910']
+    }
+  },
+  diskPnList: [
+    { brand: "Intel", pn: "SSDPF2KX076T1", Type: "NVMe/7.68TB" },
+    { brand: "Intel", pn: "SSDPF2KX038T1", Type: "NVMe/3.84TB" },
+    { brand: "KIOXIA", pn: "KCD81RUG3T84", Type: "NVMe/3.84TB" },
+    { brand: "KIOXIA", pn: "KCD81RUG7T68", Type: "NVMe/7.68TB" },
+    { brand: "KIOXIA", pn: "KCD81RUG1T92", Type: "NVMe/1.92TB" },
+    { brand: "Samsung", pn: "MZQL21T9HCJR-00B7C", Type: "NVMe/1.92TB" },
+    { brand: "Samsung", pn: "MZQL23T8HCLS-00B7C", Type: "NVMe/3.84TB" },
+    { brand: "Seagate", pn: "ST20000NM007D", Type: "HDD/20TB" },
+    { brand: "Seagate", pn: "ST8000NM017B", Type: "HDD/8TB" },
+    { brand: "Union Memory", pn: "UP2A63T8SD004LX", Type: "NVMe/3.84TB" },
+    { brand: "Western Digital", pn: "WUH722020BLE604", Type: "HDD/20TB" },
+    { brand: "Western Digital", pn: "HUS728T8TALE6L4", Type: "HDD/8TB" }
+  ]
+};
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    Promise.all([
+      // 缓存静态资源
+      caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(urlsToCache)),
+      // 缓存配件数据
+      caches.open('parts-data-v1')
+        .then(cache => cache.put('/pda_pwa/parts-data', new Response(JSON.stringify(partsData))))
+    ]).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    Promise.all([
+      // 立即接管所有客户端
+      self.clients.claim(),
+      // 清理旧缓存
+      caches.keys().then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheName !== CACHE_NAME && cacheName !== 'parts-data-v1') {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    ])
   );
 });
 
 self.addEventListener('fetch', event => {
+  // 处理配件数据请求
+  if (event.request.url.endsWith('/parts-data')) {
+    event.respondWith(
+      caches.match('/pda_pwa/parts-data')
+        .then(response => response || fetch(event.request))
+    );
+    return;
+  }
+
+  // 处理其他请求
   event.respondWith(
     caches.match(event.request)
       .then(response => {
@@ -45,19 +123,5 @@ self.addEventListener('fetch', event => {
             return response;
           });
       })
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
 }); 
